94a25e01ffcf75e19049b484cc08d2d9cfeae74f
Asus <Asus@DESKTOP-38C8QDK>
2024-03-16 01:56:24 +0200

    Add project files.

---
+++ Laba3/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Laba3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n-----Task1-----\n");
+            int[] numbers = { 1, 2, 3, 4, 5, 2, 3, 6, 7, 8, 9, 1 };
+            Task.Num1(numbers);
+            Console.WriteLine("\n---------------\n");
+
+
+            Console.WriteLine("\n-----Task2-----\n");
+            int[] array = { 1, 3, 5, 7, 9, 2, 4, 6, 8 };
+
+            Console.WriteLine("Enter a threshold value:");
+            int threshold = int.Parse(Console.ReadLine());
+
+            int count = Task.Num2(array, threshold);
+            Console.WriteLine($"The number of values ​​in the array is less {threshold}: {count}");
+            Console.WriteLine("\n---------------\n");
+
+
+            Console.WriteLine("\n-----Task3-----\n");
+            Console.WriteLine("Enter three numbers separated by a space:");
+            string input = Console.ReadLine();
+            string[] inputNumbers = input.Split(' ');
+
+            if (inputNumbers.Length != 3)
+            {
+                Console.WriteLine("Invalid input format.");
+                return;
+            }
+
+            int num1 = int.Parse(inputNumbers[0]);
+            int num2 = int.Parse(inputNumbers[1]);
+            int num3 = int.Parse(inputNumbers[2]);
+
+            int[] mas = { 7, 6, 5, 3, 4, 7, 6, 5, 8, 7, 6, 5 };
+
+            int count2 = Task.Num3(mas, num1, num2, num3);
+
+            Console.WriteLine($"The number of repetitions of the sequence: {count}");
+            Console.WriteLine("\n---------------\n");
+
+
+            Console.WriteLine("\n-----Task4-----\n");
+            int[] array1 = { 1, 2, 3, 4, 5 };
+            int[] array2 = { 3, 4, 5, 6, 7 };
+
+            int[] resultArray = Task.Num4(array1, array2);
+
+            Console.WriteLine("Resulting Array:");
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                Console.Write(resultArray[i] + " ");
+            }
+            Console.WriteLine("\n---------------\n");
+
+
+            Console.WriteLine("\n-----Task5-----\n");
+            int[,] mas1 = {
+                { 31, 17, 52 },
+                { 39, 64, 96 },
+                { 10, 28, 35 }
+            };
+            Task.Num5(mas1);
+            Console.WriteLine("\n---------------\n");
+
+
+            Console.WriteLine("\n-----Task6-----\n");
+            Console.WriteLine("Enter a sentence:");
+            string sentence = Console.ReadLine(); // Зчитуємо речення з клавіатури
+
+            int wordCount = Task.Num6(sentence); // Викликаємо метод для підрахунку слів у реченні
+
+            Console.WriteLine($"Number of words in a sentence: {wordCount}");
+            Console.WriteLine("\n---------------\n");
+
+        }
+    }
+}
+
---
+++ Laba3/Task.cs
@@ -0,0 +1,135 @@
+﻿namespace Laba3
+{
+    public class Task
+    {
+        public static void Num1(int[] numbers)
+        {
+            int evenCount = numbers.Count(n => n % 2 == 0);
+
+            int oddCount = numbers.Count(n => n % 2 != 0);
+
+            int uniqueCount = numbers.Distinct().Count();
+
+            Console.WriteLine($"Even numbers: {evenCount}");
+            Console.WriteLine($"Odd numbers: {oddCount}");
+            Console.WriteLine($"Unique numbers: {uniqueCount}");
+        }
+        public static int Num2(int[] array, int threshold)
+        {
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < threshold)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public static int Num3(int[] array, int num1, int num2, int num3)
+        {
+            int count = 0;
+
+            for (int i = 0; i < array.Length - 2; i++)
+            {
+                if (array[i] == num1 && array[i + 1] == num2 && array[i + 2] == num3)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        public static int[] Num4(int[] array1, int[] array2)
+        {
+            // Знаходимо розмірність третього масиву
+            int count = 0;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    if (array1[i] == array2[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            // Створюємо третій масив
+            int[] result = new int[count];
+            int index = 0;
+
+            // Заповнюємо третій масив
+            for (int i = 0; i < array1.Length; i++)
+            {
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    if (array1[i] == array2[j])
+                    {
+                        result[index++] = array1[i];
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+        public static void Num5(int[,] array)
+        {
+            int min = array[0, 0]; 
+            int max = array[0, 0];
+
+            int rows = array.GetLength(0);
+            int columns = array.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    int current = array[i, j];
+
+                    
+                    if (current < min)
+                    {
+                        min = current; 
+                    }
+
+                   
+                    if (current > max)
+                    {
+                        max = current;
+                    }
+                }
+            }
+
+            Console.WriteLine("Minimum value: " + min);
+            Console.WriteLine("Maximum value: " + max);
+        }
+        public static int Num6(string sentence)
+        {
+            int wordCount = 0;
+            bool inWord = false;
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                // Якщо поточний символ - буква чи цифра, і ми не знаходимось у слові,
+                // то це початок нового слова
+                if (char.IsLetterOrDigit(sentence[i]) && !inWord)
+                {
+                    inWord = true; 
+                    wordCount++; 
+                }
+                // Якщо поточний символ - не буква чи цифра, і ми знаходимось у слові,
+                // то це кінець слова
+                else if (!char.IsLetterOrDigit(sentence[i]) && inWord)
+                {
+                    inWord = false;
+                }
+            }
+
+            return wordCount;
+        }
+    }
+}
